5a086f04b5c3f40b8b0046e97577cab2cfe487ac
Nivine20 <nivine.diallo@edu.ece.fr>
2020-10-08 16:03:26 +0200

    questions 1-2-3

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
 
 // Fonction appelée lors du click du bouton
-function start() {
+function start(city) {
   // Création de l'objet apiWeather
-  const apiWeather = new API_WEATHER();
+  const apiWeather = new API_WEATHER(city);
   // Appel de la fonction fetchTodayForecast
 
   apiWeather
@@ -22,10 +22,18 @@ function start() {
       document.getElementById('today-forecast-more-info').innerHTML = description;
       document.getElementById('icon-weather-container').innerHTML = icon;
       document.getElementById('today-forecast-temp').innerHTML = `${temp}°C`;
-      
     })
     .catch(function(error) {
       // Affiche une erreur
       console.error(error);
     });
+
 }
+    function getCity() {
+
+      city = document.getElementById('city-input').value;
+      start(city);
+
+  }
+  
+
